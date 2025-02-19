98d445487cebc8913c0465a91b7b17231e34e84e
abreeu <guilherme95abreu@hotmail.com>
2025-02-19 13:46:37 -0300

    Alteração do botão de mostra copiar

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
 import { useState } from "react"
 import passwordImg from './assets/password.png'
 
-
-
-
-
 function App() {
- const [password, setPassword] = useState ("")
- const [copyText, setCopyText] = useState("Copiar")
- const [passwordSize, setPasswordSize]= useState(8)
- function generate (){
+  const [password, setPassword] = useState("")
+  const [copyText, setCopyText] = useState("Copiar")
+  const [passwordSize, setPasswordSize] = useState(8)
 
-  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numbers = "0123456789";
-  const specialChars ="!@#$%&"
-  let newPassword = ""
+  function generate() {
+    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const specialChars = "!@#$%&"
+    let newPassword = ""
 
-  newPassword += specialChars[Math.floor(Math.random() * specialChars.length)];
+    newPassword += specialChars[Math.floor(Math.random() * specialChars.length)];
 
-   const allCharacters = lowerCase + upperCase + numbers;
-   
-   for (let i = 1; i < passwordSize; i++) {
-    const position = Math.floor(Math.random() * allCharacters.length);
-    newPassword += allCharacters[position];
+    const allCharacters = lowerCase + upperCase + numbers;
+
+    for (let i = 1; i < passwordSize; i++) {
+      const position = Math.floor(Math.random() * allCharacters.length);
+      newPassword += allCharacters[position];
+    }
+    setPassword(newPassword)
+    setCopyText("Copiar")
   }
-   setPassword(newPassword)
-   setCopyText("Copiar")
- }
 
- function copyToClipBoard (){
-  window.navigator.clipboard.writeText(password)
-  setCopyText("Copiado!")
- }
+  function copyToClipBoard() {
+    window.navigator.clipboard.writeText(password)
+    setCopyText("Copiado!")
+  }
+
   return (
     <div>
       <img src={passwordImg} alt="password" />
@@ -40,22 +37,24 @@ function App() {
       <div>
         <label htmlFor="passwordSize">Tamanho:</label>
         <input 
-        type="number" 
-        id="passwordSize" 
-        min={1} 
-        max={20}
-        value={passwordSize}
-        onChange={(ev)=>setPasswordSize(ev.target.value)}
+          type="number" 
+          id="passwordSize" 
+          min={1} 
+          max={20}
+          value={passwordSize}
+          onChange={(ev) => setPasswordSize(ev.target.value)}
         />
-        
       </div>
       <button onClick={generate}>Gerar senha de {passwordSize} caracteres !</button>
-      <button onClick={copyToClipBoard}>{copyText}</button>
+
+      
+      {password && (
+        <button onClick={copyToClipBoard}>{copyText}</button>
+      )}
+
       <div>{password}</div>
     </div>
   )
 }
 
 export default App
-
-
